d29a31549dd62cad42a66ecfb95c04dcd13c78ce
adaptiman <david@thesweeneys.org>
2024-05-26 12:17:59 -0500

    initial commit

---
+++ getSSS.py
@@ -0,0 +1,4 @@
+import substack_api as ss
+from substack_api import newsletter, user
+print(newsletter.list_all_categories())
+#print(newsletter.get_newsletters_in_category(4, start_page=0, end_page=2))
